2518d33e662fcb8b00e151ca1cfc2cd96ab4932d
Harrison Russell-Pribnow <har1@iastate.edu>
2023-03-30 12:28:09 -0500

    update

--- Hw3/task2build.py
+++ Hw3/task2build.py
@@ -2,11 +2,40 @@ import argparse
 import json
 import time
 import pyspark
+from itertools import combinations
 
 
-def main(train_file, model_file, co_rated_thr, sc):
+def main(train_file, model_file, co_rated_thr, sc : pyspark.SparkContext):
 
-    """ you need to write your own code """
+    review_rdd = sc.textFile(train_file).map(lambda x: json.loads(x))
+    user_review_matrix_rdd = review_rdd.map(lambda x: (x['user_id'],x['business_id'])).aggregateByKey([], lambda a,b: a + [b], lambda a,b: a + b)\
+                                .map(lambda x: (x[0], [*set(x[1])]))
+    co_rated_rdd = user_review_matrix_rdd.flatMap(lambda x: map_co_rated(x)).reduceByKey(lambda a,b:a+b).filter(lambda x: x >= co_rated_thr)
+    bis_review_matrix_rdd = review_rdd.map(lambda x: (x['business_id'],(x['user_id'], x['stars']))).aggregateByKey([], lambda a,b: a + [b], lambda a,b: a + b)
+    bis_review_matrix_rdd = bis_review_matrix_rdd
+    user_set = review_rdd.map(lambda x: x['user_id']).distinct().collect()
+    sig_matrix_rdd = review_matrix_rdd.map(lambda x: map_to_matrix(x, bisness_set))
+    
+
+
+def map_to_matrix(line, bisness_set):
+    user_id = line[0]
+    ratings_list = line[1]
+    rating_output = []
+    seen = []
+    for rating in ratings_list:
+        if rating[0] not in seen:
+            rating_output.append(rating)
+            seen.append(rating[0])
+    return (user_id, rating_output)
+
+
+def map_co_rated(line):
+    ratings_list = line[1]
+    if len(ratings_list) < 2: return []
+    pairs = combinations(ratings_list,2)
+    return [(tuple(sorted(pair, key=lambda x: x[0])),1) for pair in pairs]
+
 
 
 if __name__ == '__main__':
