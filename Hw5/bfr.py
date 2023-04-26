9ae653f9a06e9a60f7e5abcc6b6b66ef0b07bcbe
HarryR-P <har1@iastate.edu>
2023-04-26 12:54:24 -0500

    update

--- Hw5/bfr.py
+++ Hw5/bfr.py
@@ -21,23 +21,30 @@ def main(input_path, n_cluster, out_file1, out_file2):
 
 
 def h_cluster(data_df : pd.DataFrame, max_dist = 20):
-    points = data_df.to_dict(orient='list')
-    while len(points) > 1:
+    points_df = data_df.copy(deep=True)
+    point2id = {(i,):i for i in points_df.index.tolist()}
+    max_idx = len(point2id) - 1
+    while len(point2id) > 1:
         min_dist = float('inf')
         min_points = -1
-        for p1, p2 in combinations(points.keys(),2):
-            dist = calc_dist(points[p1],points[p2])
+        for p1, p2 in combinations(point2id.keys(),2):
+            dist = calc_dist(points_df.iloc[point2id[p1]],points_df.iloc[point2id[p2]])
             if min_dist > dist:
                 min_dist = dist
-                min_points = (*sorted(p1,p2),)
+                min_points = (p1,p2)
         if min_dist > max_dist:
             break
-        centroid = list(np.average([points[min_points[0]],points[min_points[1]]],axis=0))
-        points[min_points] = centroid
-        del points[min_points[0]]
-        del points[min_points[1]]
+        centroid = list(np.average([points_df.iloc[point2id[min_points[0]]].to_numpy(),points_df.iloc[point2id[min_points[1]]].to_numpy()],axis=0))
+        
+        row = pd.Series({'id':max_idx + 1,'x0':centroid[0],'x1':centroid[1],'x2':centroid[2],'x3':centroid[3],'x4':centroid[4],'x5':centroid[5],'x6':centroid[6],'x7':centroid[7],'x8':centroid[8],
+                         'x9':centroid[9]}).to_frame().T.set_index('id')
+        points_df = pd.concat([points_df, row])
+        point2id[min_points] = max_idx + 1
+        for point in min_points:
+            del point2id[point]
+        max_idx += 1
 
-    return points.keys()
+    return [flatten(cluster) for cluster in point2id.keys()]
 
 
 def calc_dist(p1, p2):
@@ -45,6 +52,14 @@ def calc_dist(p1, p2):
     for d1, d2 in zip(p1,p2):
         dist += (d1 - d2)**2
     return np.sqrt(dist).item()
+
+
+def flatten(nestedlist):
+    if not(bool(nestedlist)):
+        return nestedlist
+    if isinstance(nestedlist[0], tuple):
+        return flatten(*nestedlist[:1]) + flatten(nestedlist[1:])
+    return nestedlist[:1] + flatten(nestedlist[1:])
 
 
 def seperate_clusters(clusters, data, n_cluster):
--- Hw5/test.py
+++ Hw5/test.py
@@ -17,9 +17,9 @@ def main():
     return
 
 
-def h_cluster(data_df : pd.DataFrame, max_dist = 500):
+def h_cluster(data_df : pd.DataFrame, max_dist = 20):
     points_df = data_df.copy(deep=True)
-    point2id = {i:i for i in points_df.index.tolist()}
+    point2id = {(i,):i for i in points_df.index.tolist()}
     max_idx = len(point2id) - 1
     while len(point2id) > 1:
         min_dist = float('inf')
@@ -28,21 +28,20 @@ def h_cluster(data_df : pd.DataFrame, max_dist = 500):
             dist = calc_dist(points_df.iloc[point2id[p1]],points_df.iloc[point2id[p2]])
             if min_dist > dist:
                 min_dist = dist
-                min_points = (*sorted((p1,p2)),)
-        print(min_dist)
+                min_points = (p1,p2)
         if min_dist > max_dist:
             break
         centroid = list(np.average([points_df.iloc[point2id[min_points[0]]].to_numpy(),points_df.iloc[point2id[min_points[1]]].to_numpy()],axis=0))
         
         row = pd.Series({'id':max_idx + 1,'x0':centroid[0],'x1':centroid[1],'x2':centroid[2],'x3':centroid[3],'x4':centroid[4],'x5':centroid[5],'x6':centroid[6],'x7':centroid[7],'x8':centroid[8],
-                         'x9':centroid[9]})
-        points_df = pd.concat([points_df, row.to_frame().T])
+                         'x9':centroid[9]}).to_frame().T.set_index('id')
+        points_df = pd.concat([points_df, row])
         point2id[min_points] = max_idx + 1
-        del point2id[min_points[0]]
-        del point2id[min_points[1]]
+        for point in min_points:
+            del point2id[point]
         max_idx += 1
 
-    return point2id.keys()
+    return [flatten(cluster) for cluster in point2id.keys()]
 
 
 def calc_dist(p1, p2):
@@ -50,6 +49,14 @@ def calc_dist(p1, p2):
     for d1, d2 in zip(p1,p2):
         dist += (d1 - d2)**2
     return np.sqrt(dist).item()
+
+
+def flatten(nestedlist):
+    if not(bool(nestedlist)):
+        return nestedlist
+    if isinstance(nestedlist[0], tuple):
+        return flatten(*nestedlist[:1]) + flatten(nestedlist[1:])
+    return nestedlist[:1] + flatten(nestedlist[1:])
 
 
 if __name__ == '__main__':
